982809d3b906edddd49dd8d8c7c10f6f941a6372
mjdele <mike.delehant@gmail.com>
2016-09-29 18:41:40 -0400

    Added bye method to CLI, fixed exit repetition problem with when conditional

--- lib/election_cli_gem/cli.rb
+++ lib/election_cli_gem/cli.rb
@@ -2,7 +2,7 @@ class ElectionCliGem::CLI
   def call
     national_poll
     state_breakdown
-    
+    bye
   end
 
   def national_poll
@@ -20,11 +20,15 @@ class ElectionCliGem::CLI
       case input
       when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
         puts "MY CASE STATEMENT IS WORKING!!!"
-        
+      when "exit"
       else
         puts "Please check your spelling or type exit."
       end
     end
+  end
+
+  def bye
+    puts "New polls are always coming in. Check back in a few hours to see updated results."
   end
 
 
