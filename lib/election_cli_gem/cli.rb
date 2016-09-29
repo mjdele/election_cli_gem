553a58fa5f317349c5c445f1914af604375edc53
mjdele <mike.delehant@gmail.com>
2016-09-29 19:24:42 -0400

    Added blank string thorughout CLI to help with readability

--- lib/election_cli_gem/cli.rb
+++ lib/election_cli_gem/cli.rb
@@ -6,12 +6,14 @@ class ElectionCliGem::CLI
   end
 
   def national_poll
-    puts "Who Will Win the Presidency?"
+    puts ""
+    puts "WHO WILL WIN THE PRESIDENCY?"
     puts "Today's FiveThirtyEight.com Polling Update:"
     ElectionCliGem::Polls.scrape_national  
   end
 
   def state_breakdown
+    puts ""
     puts "Enter a state name to see individual state predictions (for D.C., use 'district of columbia') or type exit:"
     input = nil
     while input != "exit"
@@ -19,7 +21,9 @@ class ElectionCliGem::CLI
 
       case input
       when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
+        puts ""
         ElectionCliGem::Polls.scrape_state(input)
+        puts ""
         puts "Enter another state or type exit:"
       when "exit"
       else
@@ -29,7 +33,9 @@ class ElectionCliGem::CLI
   end
 
   def bye
+    puts ""
     puts "New polls are always coming in. Check back in a few hours to see updated results."
+    puts ""
   end
 
 
