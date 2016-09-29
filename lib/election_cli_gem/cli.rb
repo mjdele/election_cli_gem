cf8c6361a8cdb4ed6907344eab1999d131d20fbd
mjdele <mike.delehant@gmail.com>
2016-09-29 19:13:29 -0400

    Added placekeeping string to #scrape_state, finished #scrape_national scraping and output is accurate

--- lib/election_cli_gem/cli.rb
+++ lib/election_cli_gem/cli.rb
@@ -19,7 +19,7 @@ class ElectionCliGem::CLI
 
       case input
       when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
-        puts "MY CASE STATEMENT IS WORKING!!!"
+        ElectionCliGem::Polls.scrape_state(input)
       when "exit"
       else
         puts "Please check your spelling or type exit."
--- lib/election_cli_gem/polls.rb
+++ lib/election_cli_gem/polls.rb
@@ -2,11 +2,18 @@ class ElectionCliGem::Polls
   attr_accessor :region, :dem_percent, :rep_percent
 
   def self.scrape_national
-    puts "I'm scraping the National poll!!!"
+    html = "http://projects.fivethirtyeight.com/2016-election-forecast/"
+    doc = Nokogiri::HTML(open(html))
+
+    poll = self.new
+    poll.dem_percent = doc.search("div.cardset.current div.candidate.one.dem  p.candidate-val.winprob").text
+    poll.rep_percent = doc.search("div.cardset.current div.candidate.three.rep p.candidate-val.winprob").text
+
+    puts "NATIONAL FORECAST --> Hillary Clinton: #{poll.dem_percent} / Donald Trump: #{poll.rep_percent}"
   end
 
   def self.scrape_state(state)
-    
+    puts "MY STATE SCRAPER IS WORKING!!!"
   end
 
 
