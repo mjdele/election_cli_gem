d2195789644fab78520fd4729ba801c5593e2b0c
mjdele <mike.delehant@gmail.com>
2016-09-29 18:33:57 -0400

    Added string place holder for state scraper and an else statement for misspellings in CLI case statement

--- lib/election_cli_gem/cli.rb
+++ lib/election_cli_gem/cli.rb
@@ -16,10 +16,15 @@ class ElectionCliGem::CLI
     input = nil
     while input != "exit"
       input = gets.strip.downcase.gsub(" ", "-")
+
       case input
-        when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
-          
-    
+      when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
+        puts "MY CASE STATEMENT IS WORKING!!!"
+        
+      else
+        puts "Please check your spelling or type exit."
+      end
+    end
   end
 
 
