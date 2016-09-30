8d2701f2713732f812c883da0db3ea9c1a7d9ac7
mjdele <mike.delehant@gmail.com>
2016-09-30 16:47:28 -0400

    Added scraper.rb dependency in election_cli_gem.rb, renamed national and state scraper calls within CLI to reflect refactoring changes

--- lib/election_cli_gem.rb
+++ lib/election_cli_gem.rb
@@ -5,4 +5,5 @@ require 'pry'
 require_relative "./election_cli_gem/version"
 require_relative "./election_cli_gem/cli"
 require_relative "./election_cli_gem/polls"
+require_relative "./election_cli_gem/scraper"
 
--- lib/election_cli_gem/cli.rb
+++ lib/election_cli_gem/cli.rb
@@ -10,7 +10,7 @@ class ElectionCliGem::CLI
     puts "WHO WILL WIN THE PRESIDENCY?"
     puts "Today's FiveThirtyEight.com Polling Update:"
     puts ""
-    ElectionCliGem::Polls.scrape_national  
+    ElectionCliGem::Scraper.new.scrape_national  
   end
 
   def state_breakdown
@@ -23,7 +23,7 @@ class ElectionCliGem::CLI
       case input
       when "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "district-of-columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new-hampshire", "new-jersey", "new-mexico", "new-york", "north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"
         puts ""
-        ElectionCliGem::Polls.scrape_state(input)
+        ElectionCliGem::Scraper.new.scrape_state(input)
         puts ""
         puts "Enter another state or type exit:"
       when "exit"
